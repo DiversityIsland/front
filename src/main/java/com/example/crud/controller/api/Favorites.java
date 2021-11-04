eca5b68a7821211a97d09fd95e2be1c5fc7f817d
Александр Моисеев <sobergrim@gmail.com>
2021-11-04 23:52:33 +0300

    v 1.25

--- src/main/java/com/example/crud/controller/api/Favorites.java
+++ src/main/java/com/example/crud/controller/api/Favorites.java
@@ -9,6 +9,16 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api/favorites")
 public class Favorites {
 
+    @GetMapping()
+    public String getCheckItem1() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<>("", headers);
+        ResponseEntity<String> respEntity = new RestTemplate().exchange("http://192.168.1.38:8080/api/item/popular/", HttpMethod.GET, entity, String.class);
+        return respEntity.getBody();
+    }
+
     @GetMapping("/existsItem/{id}")
     public String getCheckItem(@PathVariable long id) {
         HttpHeaders headers = new HttpHeaders();
--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -7,20 +7,47 @@ async function update() {
             '<li class="nav-item"><a id='+categories[i].id+' href="#" class="nav-link link-dark sideMenu" aria-current="page">'+categories[i].name+'</a></li>';
     }
 
+
+    /**
+     * обработчик
+     * обработчик на все кнопки в навбар кроме "популярное"
+     */
     $('.sideMenu').on('click', function () {
         //переключение стилей кнопок навбара и контейнера с контентом
         toggleClassesContent($(this));
         toggleClassesNavBar($(this));
         //перезапись категории в заголовок
         let category = $(this).html();
-        $('#dinamic_head').html(category);
+        dinamic_head.html(category);
         //перезапись динамического контента, товары по категории
         searchByCategory($(this));
+    })
+
+    /**
+     * обработчик
+     * обработчик на кнопке "популярное" в навбар
+     */
+    $('#popular_show_btn').on('click', function () {
+        popular_show_content.addClass('show');
+        dinamic_show_content.removeClass('show');
+        dinamic_inner_content.html("");
+
+        //toggleClassesNavBar($(this));
+        $('.active').removeClass('active').addClass('link-dark');
+        $(this).removeClass('link-dark');
+        $(this).addClass('active');
     })
 }
 update();
 
+async function updateFav() {
+    let response = await fetch('/api/favorites');
+    let categories = await response.json();
 
-
-
+    for (let i = 0; i < categories.length; i++) {
+        document.getElementById("items").innerHTML +=
+            '123';
+    }
+}
+updateFav();
 
