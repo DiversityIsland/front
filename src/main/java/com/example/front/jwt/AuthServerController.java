a5c1c9930f373f601727436936681cf8e2ee1f62
Rudnev, Prokhor <rudnpro@hilti.com>
2022-01-13 17:01:53 +0300

    13.03 JSON still not done

--- src/main/java/com/example/front/jwt/AuthServerController.java
+++ src/main/java/com/example/front/jwt/AuthServerController.java
@@ -1,6 +1,7 @@
 package com.example.front.jwt;
 
 import com.example.front.controller.RestService;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
 
 
 @RestController
@@ -24,29 +26,35 @@ public class AuthServerController {
      * http://localhost/api/authserver?username=ADMIN&password=ADMIN
      * контракт: (username, password -> id, username, roles, error)
      * */
-    //Jackson ObjectMapper  reaDVALUE
 
     @GetMapping("/authserver")
     public String jwtAuth(HttpServletResponse response, HttpServletRequest request, @RequestParam("username") String username,
                           @RequestParam("password") String password) {
 
         String url = "http://localhost:8888/authserver?username="+username+"&password="+password;
-        System.out.println("url string is: " + url);
         String test = RestService.getJSON(url).getBody();
 
         JSONObject obj = new JSONObject(test);
         int id = obj.getInt("id");
         String uname = obj.getString("username");
+
+        JSONArray rolesArr = (JSONArray) obj.get("roles");
+        JSONObject allRoles = null;
+        Iterator rolesItr = rolesArr.iterator();
+        while (rolesItr.hasNext()){
+            allRoles = (JSONObject) rolesItr.next();
+        }
+
+        System.out.println("rolesarray: " + rolesArr);
+        System.out.println("allroles: " + allRoles.toString());
+
         Object urole = obj.getJSONArray("roles").get(0);
-        System.out.println("JSON ROLES: " + urole);
         JSONObject obj2 = new JSONObject(urole.toString());
         String role = obj2.getString("name");
 
         System.out.println( "JSON id: " + id + " JSON name: " + uname + " JSON role: " + role);
-        System.out.println("AuthServerController");
         return "{\"id\":\"" + id + "\",\"username\":\"" + uname + "\",\"roles\":[\"ROLE_" + role + "\"],\"error\":\"\"}";
 
-        //todo заглушка на сервер авторизации
         //return "{\"id\":\"1\",\"username\":\"ADMIN\",\"roles\":[\"ROLE_ADMIN\",\"ROLE_USER\"],\"error\":\"\"}";
 
     }
