dce405a34004a85d27fd48bdfd5111ee6a870dbe
Kulakov Pavel <kombojiec@gmail.com>
2022-01-17 19:40:15 +0300

    moderator(feat): create API for moderator requests (ModeratorItems.class, ModeratorReviews.class, ModeratorShops.class) and add putJSON method in RestService.class

--- src/main/webapp/js/moderator/ModeratorApi.js
+++ src/main/webapp/js/moderator/ModeratorApi.js
@@ -1,6 +1,6 @@
 class ModeratorApi {
     constructor() {
-        this._url = 'http://localhost:8888/moderator/api';
+        this._url = 'http://localhost';
         this._head = {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
@@ -17,22 +17,22 @@ class ModeratorApi {
 
     // =========================== Запросы на товары ===========================
     getUnmoderatedItems() {
-        return fetch(this._url + `/items/getUnmoderatedItems`)
+        return fetch(this._url + `/moderate/items`)
             .then(res => this._checkResponse(res))
     }
 
     getUnmoderatedItem(id) {
-        return fetch(this._url + `/items/getOneUnmoderatedItem/${id}`)
+        return fetch(this._url + `/moderate/items/getOneUnmoderatedItem/${id}`)
             .then(res => this._checkResponse(res))
     }
 
     getUnmoderatedItemsCount() {
-        return fetch(this._url + `/items/getUnmoderatedItemsCount`)
+        return fetch(this._url + `/moderate/items/getUnmoderatedItemsCount`)
             .then(res => this._checkResponse(res))
     }
 
     updateItem(item) {
-        return fetch(this._url + `/items/editItem`, {
+        return fetch(this._url + `/moderate/items/editItem`, {
             method: 'PUT',
             headers: this._head,
             body: JSON.stringify(item)
@@ -43,23 +43,23 @@ class ModeratorApi {
 
     // =========================== Запросы на магазины ===========================
     getUnmoderatedShops(){
-        return fetch(this._url + `/shops/getUnmoderatedShops`)
+        return fetch( this._url + `/moderate/shops`)
             .then(res => this._checkResponse(res))
     }
 
     getUnmoderatedShop(id){
-        return fetch(this._url + `/shops/getOneUnmoderatedShop/${id}`)
+        return fetch(this._url + `/moderate/shops/getOneUnmoderatedShop/${id}`)
             .then(res => this._checkResponse(res))
     }
 
     getUnmoderatedShopsCount(){
-        return fetch(this._url + `/shops/getUnmoderatedShopsCount`)
+        return fetch(this._url + `/moderate/shops/getUnmoderatedShopsCount`)
             .then(res => this._checkResponse(res))
     }
 
     updateShop(shop) {
         console.log(shop)
-        return fetch(this._url + `/shops/editShop`, {
+        return fetch(this._url + `/moderate/shops/editShop`, {
             method: 'PUT',
             headers: this._head,
             body: JSON.stringify(shop)
@@ -70,29 +70,28 @@ class ModeratorApi {
 
     // =========================== Запросы на отзывы ===========================
     getUnmoderatedReviews(){
-        return fetch(this._url + `/reviews/getUnmoderatedReviews`)
+        return fetch(this._url + `/moderate/reviews`)
             .then(res => this._checkResponse(res))
     }
 
     getUnmoderatedReview(id){
-        return fetch(this._url + `/reviews/getOneUnmoderatedReview/${id}`)
+        return fetch(this._url + `/moderate/reviews/getOneUnmoderatedReview/${id}`)
             .then(res => this._checkResponse(res))
     }
 
     getUnmoderatedReviewsCount(){
-        return fetch(this._url + `/reviews/getUnmoderatedReviewsCount`)
+        return fetch(this._url + `/moderate/reviews/getUnmoderatedReviewsCount`)
             .then(res => this._checkResponse(res))
     }
 
     updateReview(review) {
-        return fetch(this._url + `/reviews/editReview`, {
+        return fetch(this._url + `/moderate/reviews/editReview`, {
             method: 'PUT',
             headers: this._head,
             body: JSON.stringify(review)
         })
             .then(res => this._checkResponse(res))
     }
-
 
 }
 
